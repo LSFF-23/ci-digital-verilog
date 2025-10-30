52f1f741e37f713b4a590052afdb0fcffd1b3ef5
Luis Soares <luisfelipe.fsk@gmail.com>
2025-10-30 19:52:23 -0300

    Update mux4to1.v

--- circuitos-digitais-i/mux4to1.v
+++ circuitos-digitais-i/mux4to1.v
@@ -1,29 +1,37 @@
 module mux4to1 #(parameter N = 8) (
-    input [3:0][N-1:0] in,
+    input [N-1:0] D3, D2, D1, D0,
     input [1:0] sel,
     output reg [N-1:0] out
 );
 
-always @* out = in[sel];
+always @* begin
+    case (sel)
+        2'b00: out = D0;
+        2'b01: out = D1;
+        2'b10: out = D2;
+        2'b11: out = D3;
+        default: out = {N{1'bx}};
+    endcase
+end
 
 endmodule
 
 module mux4to1_tb;
 parameter N = 4;
-reg [3:0][N-1:0] in;
+reg [N-1:0] D3, D2, D1, D0;
 reg [1:0] sel;
 wire [N-1:0] out;
 
-mux4to1#(N) dut (in, sel, out);
+mux4to1#(N) dut (D3, D2, D1, D0, sel, out);
 
 always #1 sel[0] = !sel[0];
 always #2 sel[1] = !sel[1];
 
 initial begin
-    $display("in[3] | in[2] | in[1] | in[0] | sel | out ");
-    $monitor("%5b | %5b | %5b | %5b | %3b | %b", in[3], in[2], in[1], in[0], sel, out);
+    $display("  D3  |  D2  |  D1  |  D0  | sel | out ");
+    $monitor(" %4b | %4b | %4b | %4b | %3b | %b", D3, D2, D1, D0, sel, out);
     sel = 0;
-    in[3] = 4'b1111; in[2] = 4'b0111; in[1] = 4'b0011; in[0] = 4'b0001;
+    D3 = 4'b1111; D2 = 4'b0111; D1 = 4'b0011; D0 = 4'b0001;
     #4 $finish(0);
 end
 
