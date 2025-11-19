05cae2f9751dee0fe9c49a848d7d667e769239e3
LSFF-23 <luisfelipe.fsk@gmail.com>
2025-11-19 20:35:24 -0300

    feat(): latch rs

---
+++ circuitos-digitais-ii/latch_sr.v
@@ -0,0 +1,28 @@
+module latch_rs (r, s, qa, qb);
+input r, s;
+output qa, qb;
+
+assign qa = ~(r | qb);
+assign qb = ~(s | qa);
+
+endmodule
+
+module latch_tb;
+reg r, s;
+wire qa, qb;
+
+latch_rs dut (r, s, qa, qb);
+
+initial begin
+    $display("r s q Q");
+    $monitor("%b %b %b %b", r, s, qa, qb);
+    r = 0; s = 0;
+    #1 s = 1;
+    #1 s = 0;
+    #1 r = 1;
+    #1 r = 0;
+    #1 r = 1; s = 1;
+    #1 $finish(0);
+end
+
+endmodule
