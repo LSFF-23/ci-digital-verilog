4e703f3e309ca6988998a5c89dc727f4529525a8
LSFF-23 <luisfelipe.fsk@gmail.com>
2025-12-03 19:29:48 -0300

    feat(): fsm - 3 cycle laser

---
+++ circuitos-digitais-ii/laser.v
@@ -0,0 +1,39 @@
+module laser (clk, rst, btn, led);
+input clk, rst, btn;
+output reg led;
+
+localparam OFF = 0;
+localparam LIG1 = 1;
+localparam LIG2 = 2;
+localparam LIG3 = 3;
+
+reg [1:0] state, next_state;
+
+
+always @(posedge clk or posedge rst) begin
+    if (rst)
+        state <= OFF;
+    else
+        state <= next_state;
+end
+
+
+always @* begin
+    case (state)
+        OFF: led = 0;
+        LIG1, LIG2, LIG3: led = 1;
+        default: led = 0;
+    endcase
+end
+
+always @* begin
+    case (state)
+        OFF: next_state = btn ? LIG1 : OFF;
+        LIG1: next_state = btn ? LIG1 : LIG2;
+        LIG2: next_state = btn ? LIG1 : LIG3;
+        LIG3: next_state = btn ? LIG1 : OFF;
+        default: next_state = OFF;
+    endcase
+end
+
+endmodule
